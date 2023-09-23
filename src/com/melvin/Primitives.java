3c72b27ecffb34855cea5d980b52e6602a63977d
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-23 16:51:32 +0530

    Primitives

--- src/com/melvin/Primitives.java
+++ src/com/melvin/Primitives.java
@@ -8,6 +8,8 @@ public class Primitives{
         double largeDecimalNumbers = 35355553.342342424;
         long largeIntegerNumbers = 534665454756768L;
         boolean check = true;
-        
+
+
+
     }
 }
