38afbbc34b4ed7e59bddc8018af7449d6630f74b
Melvin M George <melvinmgeorge1234@gmail.com>
2023-09-24 18:16:43 +0530

    Input

---
+++ src/com/melvin/Input.java
@@ -0,0 +1,13 @@
+package com.melvin;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.println("Enter your Input : ");
+        Scanner sc = new Scanner(System.in);
+        int rollNo = sc.nextInt();
+        System.out.println("Your roll number is : "+ rollNo);
+
+    }
+}
