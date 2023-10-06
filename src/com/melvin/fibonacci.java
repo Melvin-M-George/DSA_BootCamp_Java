c818b30021c4017a914b21b26a814b22f8fb7145
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-06 15:43:13 +0530

    fibonacci

--- src/com/melvin/fibonacci.java
+++ src/com/melvin/fibonacci.java
@@ -9,7 +9,7 @@ public class fibonacci {
         System.out.print(n1+" ");
         System.out.print(n2+" ");
 
-        for (int i = 0; i <10 ; i++) {
+        for (int i = 2; i <10 ; i++) {
             n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
