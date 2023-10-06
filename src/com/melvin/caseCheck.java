0fd215ae40d3cef621cb5c080289de9f0951408f
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-06 15:32:07 +0530

    caseCheck

--- src/com/melvin/caseCheck.java
+++ src/com/melvin/caseCheck.java
@@ -6,12 +6,10 @@ public class caseCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().trim().charAt(0);
-        System.out.println(ch);
 
         if(ch>'a' && ch<'z'){
             System.out.println("LowerCase");
-        }
-        if(ch>'A' && ch<'Z'){
+        }else{
             System.out.println("UpperCase");
         }
 
