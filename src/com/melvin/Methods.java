f98a866b418164d0268aa8652bfa6a22039bda7d
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-11 15:11:19 +0530

    Methods

--- src/com/melvin/Methods.java
+++ src/com/melvin/Methods.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class Methods {
 
-    static void sum(){
+    static void Greeting(){
+        System.out.println("Hello, Good Morning!!");
+    }
+
+    static int Sum(){
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
@@ -12,9 +16,27 @@ public class Methods {
         System.out.print("Enter second number: ");
         int num2 = sc.nextInt();
         int sum = num1 + num2;
-        System.out.println("The sum = "+sum);
+        return sum;
+
+
     }
+
+    static String greet(){
+        String greeting = "How are you?";
+        return greeting;
+    }
+
+    static int sum1(int num1,int num2){
+        return num1+num2;
+    }
+
     public static void main(String[] args) {
-        sum();
+        //int ans = Sum();
+        //System.out.println(ans);
+
+        //Greeting();
+
+        //System.out.println(greet());
+        System.out.println(sum1(10,20));
     }
 }
