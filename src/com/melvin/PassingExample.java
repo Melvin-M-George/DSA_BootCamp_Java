6a46c4a2aa6f87fbae33163db2c9b138cd665ab9
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-13 20:37:23 +0530

    PassingExample

---
+++ src/com/melvin/PassingExample.java
@@ -0,0 +1,13 @@
+package com.melvin;
+
+public class PassingExample {
+    static void greet(String name) {
+
+    }
+    public static void main(String[] args) {
+        String name = "Melvin";
+        greet(name);
+    }
+
+
+}
