dcbbd67665951c3237c5edeafe58dde648c94bd3
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-13 21:09:08 +0530

    changeValue

---
+++ src/com/melvin/changeValue.java
@@ -0,0 +1,12 @@
+package com.melvin;
+
+public class changeValue {
+    static void change(int [] numbers){
+        numbers[0] = 99;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        change(arr);
+
+    }
+}
