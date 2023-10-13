e1ca5f5833f7602691b671d0d869fb26f6993f46
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-14 01:17:05 +0530

    Scope

---
+++ src/com/melvin/Scope.java
@@ -0,0 +1,15 @@
+package com.melvin;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+    }
+
+    static void random(int marks){
+        int num = 69;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
