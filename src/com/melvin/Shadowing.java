d31e7288c50076d46d8b2b847bdb5ee1da66b376
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-14 02:31:33 +0530

    shadowing

---
+++ src/com/melvin/Shadowing.java
@@ -0,0 +1,14 @@
+package com.melvin;
+
+public class Shadowing {
+    static int x = 90; // this will be shadowed at line 8-->higher level value is shadowed
+    public static void main(String[] args) {
+        System.out.println(x);// Prints 90
+        int  x = 40;
+        System.out.println(x); // Prints 40
+        fun();// Print 90
+    }
+    static void fun(){
+        System.out.println(x);
+    }
+}
