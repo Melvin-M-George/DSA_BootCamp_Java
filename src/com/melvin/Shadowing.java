6fb641336eaa1dad415f0fb5669e6d2a5609443b
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-15 01:07:21 +0530

    shadowing

--- src/com/melvin/Shadowing.java
+++ src/com/melvin/Shadowing.java
@@ -4,7 +4,9 @@ public class Shadowing {
     static int x = 90; // this will be shadowed at line 8-->higher level value is shadowed
     public static void main(String[] args) {
         System.out.println(x);// Prints 90
-        int  x = 40;
+        int  x;
+        //System.out.println(x); Variable x is not initialized yet
+        x = 40;
         System.out.println(x); // Prints 40
         fun();// Print 90
     }
