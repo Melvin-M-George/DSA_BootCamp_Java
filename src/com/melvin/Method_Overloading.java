5ff56b9b688e0addc9303a2714fff5202208a465
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-15 01:17:58 +0530

    Method_Overloading

--- src/com/melvin/Method_Overloading.java
+++ src/com/melvin/Method_Overloading.java
@@ -1,22 +1,26 @@
 package com.melvin;
 
+import java.util.Arrays;
+
 public class Method_Overloading {
     public static void main(String[] args) {
-        fun();
         fun(78);
         fun("Melvin");
+        fun();
     }
-
-    static void fun(){
-        System.out.println("Hello");
-    }
-
     static void fun(int a){
         System.out.println(a);
     }
 
     static void fun(String name){
         System.out.println(name);
+    }
+
+    static void fun(int ...arg){
+        System.out.println(Arrays.toString(arg));
+    }
+    static void fun(String ...arg){
+        System.out.println(Arrays.toString(arg));
     }
 
 }
