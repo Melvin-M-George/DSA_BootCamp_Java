9ab5d8c89d853b38de15e426b1932ae879e6186e
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-16 02:12:43 +0530

    Arrays

--- src/com/melvin/arrays.java
+++ src/com/melvin/arrays.java
@@ -1,9 +1,11 @@
 package com.melvin;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
-public class Arrays {
+public class arrays {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // Q:Store a roll number
 //        int a = 19;
 
@@ -18,8 +20,13 @@ public class Arrays {
 
 //        String [] arr = new String[5];
 //        System.out.println(arr[2]);
-        Scanner sc = new Scanner(System.in);
+
+        //Array of Primitives
+        /*
+        
         int [] arr = new int[5];
+
+         */
 /*
         arr[0] = 23;
         arr[1] = 33;
@@ -28,16 +35,34 @@ public class Arrays {
         arr[4] = 63;
 
         System.out.println(arr[3]);
-Input using for loops
+        Input using for loops
 */
-
+        /*
         for (int i = 0; i < arr.length ; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i = 0; i < arr.length ; i++) {
-            System.out.print(arr[i]+" ");
-        }
 
+        System.out.println(Arrays.toString(arr));
+
+        */
+//        for (int i = 0; i < arr.length ; i++) {
+//            System.out.print(arr[i]+" ");
+//        }
+
+        //Enhanced for loop
+//        for(int element: arr){
+//            System.out.println(element);
+//        }
+
+
+        //Array of Objects
+
+        String [] str = new String[4];
+        for (int i = 0; i < str.length; i++){
+            str[i] = sc.next();
+        }
+        System.out.println(Arrays.toString(str));
+        
 
     }
 }
