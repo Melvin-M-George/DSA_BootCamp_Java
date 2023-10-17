ba29506f856674808ed714f69ab23497dd1cf897
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-17 21:35:32 +0530

    2-D Array

---
+++ src/com/melvin/MultiDim_Arrays.java
@@ -0,0 +1,14 @@
+package com.melvin;
+
+public class MultiDim_Arrays {
+    public static void main(String[] args) {
+        //int [][] arr = new int[3][3];
+
+        int [][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+    }
+}
