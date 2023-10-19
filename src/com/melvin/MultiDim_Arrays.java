ce8b42e1653557253885c65d0961b3e04e3a1d39
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-19 12:51:36 +0530

    2-D Array

--- src/com/melvin/MultiDim_Arrays.java
+++ src/com/melvin/MultiDim_Arrays.java
@@ -1,5 +1,6 @@
 package com.melvin;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MultiDim_Arrays {
@@ -15,16 +16,30 @@ public class MultiDim_Arrays {
 //        };
 
 
-        int [][] arr = new int[3][2];
+        int [][] arr = new int[3][3];
 
         Scanner sc = new Scanner(System.in);
-
+        // Input
         for (int row = 0; row < arr.length ; row++) {
             // for each col in every row
             for (int col = 0; col < arr[row].length ; col++) {
                 arr[row][col] = sc.nextInt();
             }
         }
+        // Output
+//        for (int row = 0; row < arr.length ; row++) {
+//            // for each col in every row
+//            for (int col = 0; col < arr[row].length ; col++) {
+//                System.out.print(arr[row][col] + " " );
+//            }
+//            System.out.println();
+//        }
+
+        for (int row = 0; row < arr.length ; row++) {
+            System.out.println(Arrays.toString(arr[row]));
+        }
+
+
 
 
     }
