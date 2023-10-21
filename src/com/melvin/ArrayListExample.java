59da8773ca87d4c9d357746839e73ef454d0e8a2
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-21 15:02:42 +0530

    ArrayList

---
+++ src/com/melvin/ArrayListExample.java
@@ -0,0 +1,12 @@
+package com.melvin;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        //Syntax of Arraylist
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+
+    }
+}
