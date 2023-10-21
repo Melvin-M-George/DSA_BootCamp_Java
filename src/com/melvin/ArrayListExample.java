270b37b17ebe186019ce8ebcececc0f5791ed514
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-21 22:18:47 +0530

    Arraylist

--- src/com/melvin/ArrayListExample.java
+++ src/com/melvin/ArrayListExample.java
@@ -11,14 +11,10 @@ public class ArrayListExample {
         list.add(4);
         list.add(67);
         list.add(89);
-
-
         //System.out.println(list.contains(4));
         System.out.println(list);
-
         list.set(0,99);
         list.remove(2);
-
         System.out.println(list);
 
 
