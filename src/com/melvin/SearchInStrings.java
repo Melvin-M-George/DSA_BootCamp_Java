c779d383e66bdbb5f9ded1a6bfa0ad2649cb68a9
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-23 14:28:20 +0530

    linear_search in strings

--- src/com/melvin/SearchInStrings.java
+++ src/com/melvin/SearchInStrings.java
@@ -2,9 +2,23 @@ package com.melvin;
 
 public class SearchInStrings {
     public static void main(String[] args) {
-        String name = "Melvin";
-        char target = 'z';
-        System.out.println(search(name,target));
+        String name = "melvin";
+        char target = 'v';
+        //System.out.println(search(name,target));
+        System.out.println(search2(name,target));
+    }
+
+    static boolean search2(String str, char target){
+        if(str.length() == 0){
+            return false;
+        }
+
+        for(char element : str.toCharArray()){
+            if(element==target){
+                return true;
+            }
+        }
+        return false;
     }
 
     static boolean search(String str, char target){
