0f456c53b41767541b9ee045ea1437e48228ea60
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-23 14:56:45 +0530

    Min element

---
+++ src/com/melvin/findMinNumber.java
@@ -0,0 +1,19 @@
+package com.melvin;
+
+public class findMinNumber {
+    public static void main(String[] args) {
+        int [] arr = { 18, 12, -7, -3333, 14, -28};
+        int ans = min(arr);
+        System.out.println(ans);
+    }
+
+    static int min(int [] arr){
+        int minimum = Integer.MAX_VALUE;
+        for(int element : arr){
+            if(element < minimum){
+                minimum = element;
+            }
+        }
+        return minimum;
+    }
+}
