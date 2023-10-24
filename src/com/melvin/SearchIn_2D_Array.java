8b2a45a3d4ea963b4ae2569f7d1654a60b6c1ef1
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-24 21:07:40 +0530

    Min element

---
+++ src/com/melvin/SearchIn_2D_Array.java
@@ -0,0 +1,29 @@
+package com.melvin;
+
+import java.util.Arrays;
+
+public class SearchIn_2D_Array {
+    public static void main(String[] args) {
+        int [][] arr = {
+                {1,2,34},
+                {4,5,6,7},
+                {7,8,9,2,},
+                {18,12}
+        };
+        int target = 18;
+        int [] ans = search(arr,target);//format of return value {row,col}
+        System.out.println(Arrays.toString(ans));
+
+    }
+
+    static int[] search(int [][] arr, int target){
+        for (int row = 0; row< arr.length; row++){
+            for(int col = 0; col< arr[row].length; col++){
+                if(arr[row][col]==target){
+                    return new int[]{row,col};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
