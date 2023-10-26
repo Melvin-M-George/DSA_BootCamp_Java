e3e486522ba9cdc109d506d0c3d2fd7a8cf32a02
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-26 21:14:49 +0530

    MaxWealth

--- src/com/melvin/MaxWealth.java
+++ src/com/melvin/MaxWealth.java
@@ -9,6 +9,7 @@ public class MaxWealth {
 
     static int maximumWealth(int[][] accounts ){
         // row = person and col = account
+
         int max = Integer.MIN_VALUE;
         for (int person = 0; person < accounts.length; person++) {
             //whenever you start a new col, take a new sum for that row
