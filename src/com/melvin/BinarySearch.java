c2939e2a11c8162116452e18d81b01d967de8aa4
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-29 19:44:54 +0530

    BinarySearch

---
+++ src/com/melvin/BinarySearch.java
@@ -0,0 +1,21 @@
+package com.melvin;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+
+    }
+
+    // return the index
+    // return -1 if the target element does not exist
+
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            // find the middle element
+            int mid = (start + end) / 2;
+        }
+    }
+}
