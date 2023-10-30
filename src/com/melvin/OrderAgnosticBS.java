f15e8cff4174001885ac81628f7d240015d1df06
Melvin M George <melvinmgeorge1234@gmail.com>
2023-10-30 10:37:20 +0530

    BinarySearch

--- src/com/melvin/OrderAgnosticBS.java
+++ src/com/melvin/OrderAgnosticBS.java
@@ -2,8 +2,9 @@ package com.melvin;
 
 public class OrderAgnosticBS {
     public static void main(String[] args) {
-        int[] arr = {70,57,34, 16,11, 9,4,1};
-        int target = 4;
+        int[] arr = {-24,-18,-5,0,1,5,9,15,78,111};
+        //int[] arr = {70,57,34, 16,11, 9,4,1};
+        int target = 0;
         int ans = orderAgnosticBS(arr,target);
         System.out.println(ans);
     }
@@ -16,7 +17,7 @@ public class OrderAgnosticBS {
 
         boolean isAsc = arr[start] < arr[end];
 
-        while (isAsc){
+        while (start<=end){
             // find the middle element
             int mid = start + (end -start) / 2;
 
@@ -24,25 +25,19 @@ public class OrderAgnosticBS {
                 return mid;
             }
 
-            if(target > mid){
-                start = mid + 1;
-            } else if (target < mid) {
-                end = mid -1;
+            if(isAsc){
+                if(target < arr[mid]){
+                    end = mid -1;
+                } else {
+                    start = mid + 1;
+                }
             }
             else {
-                return mid;
-            }
-        }
-
-        while (!isAsc){
-            int mid = end + (start-end) / 2;
-            if (target > mid){
-                end = mid -1;
-            } else if (target < mid) {
-                start = mid + 1;
-            }
-            else {
-                return mid;
+                if (target > arr[mid]){
+                    end = mid -1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
         return -1;
