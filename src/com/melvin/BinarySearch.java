c328ceb64b1d8b4c722a62e759a1dd3c7db0d14d
Melvin M George <melvinmgeorge1234@gmail.com>
2023-11-03 23:17:15 +0530

    BinarySearch

--- src/com/melvin/BinarySearch.java
+++ src/com/melvin/BinarySearch.java
@@ -17,7 +17,6 @@ public class BinarySearch {
             // find the middle element
             //int mid = (start + end) / 2; there is a possibility that start + end exceeds the range of int
             int mid = start + (end - start)/2;
-
             if(target < arr[mid]){
                 end = mid-1;
             } else if (target > arr[mid]) {
