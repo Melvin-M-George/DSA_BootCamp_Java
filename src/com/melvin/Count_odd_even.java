7b38534182f8007d36d3c0882746f7dd0a793c13
Melvin M George <melvinmgeorge1234@gmail.com>
2023-11-06 13:39:27 +0530

    count odd and even

---
+++ src/com/melvin/Count_odd_even.java
@@ -0,0 +1,22 @@
+package com.melvin;
+
+public class Count_odd_even {
+    public static void main(String[] args) {
+
+    }
+    public void countOddEven(int[] arr, int n)
+    {
+        // Code here
+        int countodd = 0;
+        int counteven = 0;
+        for(int i = 0; i < n ; i++){
+            if(arr[i] % 2 == 0){
+                counteven++;
+            }
+            else{
+                countodd++;
+            }
+        }
+        System.out.println(countodd+" "+counteven);
+    }
+}
