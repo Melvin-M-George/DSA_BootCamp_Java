a1fd119f3de686f226a52916c0a4ef62e51b312a
Melvin M George <melvinmgeorge1234@gmail.com>
2023-11-14 15:55:46 +0530

    first and last occurrence

---
+++ src/com/melvin/last_and_first_occurrence.java
@@ -0,0 +1,28 @@
+package com.melvin;
+
+public class last_and_first_occurrence {
+    public static void main(String[] args) {
+
+    }
+    public pair indexes(long v[], long x)
+    {
+        // Your code goes here
+        int first = -1;
+        int second = -1;
+        for(int i = 0; i < v.length;i++){
+            if(v[i]==x && first == -1){
+                first = i;
+                second = i;
+            }
+
+            if(v[i]==x && first!=-1 && second < i){
+                second = i;
+            }
+
+        }
+
+        pair p2 = new pair(first,second);
+        return p2;
+
+    }
+}
