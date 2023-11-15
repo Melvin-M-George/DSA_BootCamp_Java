0cef1a3dda390993330de5324f895c02570c9b53
Melvin M George <melvinmgeorge1234@gmail.com>
2023-11-15 17:01:27 +0530

    Binary search in infinite array

--- src/com/melvin/BS_Infinite_Array.java
+++ src/com/melvin/BS_Infinite_Array.java
@@ -5,9 +5,7 @@ public class BS_Infinite_Array {
 
     }
 
-    static int binarySearch(int[] arr, int target){
-        int start = 0;
-        int end = arr.length-1;
+    static int binarySearch(int[] arr, int target, int start, int end){
 
         while(start <= end){
             int mid = start + (end - start)/2;
