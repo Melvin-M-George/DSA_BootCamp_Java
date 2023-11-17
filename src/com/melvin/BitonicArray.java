66380a5687c0da1d4293884d27eccded95aab54d
Melvin M George <melvinmgeorge1234@gmail.com>
2023-11-17 13:09:48 +0530

    bitonic array

--- src/com/melvin/BitonicArray.java
+++ src/com/melvin/BitonicArray.java
@@ -2,10 +2,12 @@ package com.melvin;
 
 public class BitonicArray {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,4,3,2,1};
+        int ans = peakIndexInMountainArray(arr);
 
     }
 
-    static int binarySearch(int[] arr, int target){
+    static int peakIndexInMountainArray(int[] arr){
         int start = 0;
         int end = arr.length-1;
 
@@ -13,14 +15,15 @@ public class BitonicArray {
             // find the middle element
             //int mid = (start + end) / 2; there is a possibility that start + end exceeds the range of int
             int mid = start + (end - start)/2;
-            if(target < arr[mid]){
-                end = mid-1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
+            if(arr[mid] > arr[mid+1]){
+                //you are in the decreasing part of the array
+                //mid might be a possible ans , but look at left
+                //that is why end != mid -1
+                end = mid;
             } else {
-                return mid;
+                start = mid + 1;
             }
         }
-        return -1;
+        return start;
     }
 }
