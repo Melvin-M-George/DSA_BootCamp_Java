ed62fdea6ceb4d976198608ad57c3610b6ce81a7
Melvin M George <melvinmgeorge1234@gmail.com>
2023-11-18 21:46:30 +0530

    find in mountain array

--- src/com/melvin/Search_In_Bitonic.java
+++ src/com/melvin/Search_In_Bitonic.java
@@ -6,6 +6,12 @@ public class Search_In_Bitonic {
     }
 
     int search(int[] arr, int target){
+        int peak = peakIndexInMountainArray(arr);
+        int firstTry = orderAgnosticBS(arr,target,0,peak);
+        if(firstTry != -1){
+            return firstTry;
+        }
+        //try to search in second half
 
     }
     static int peakIndexInMountainArray(int[] arr) {
@@ -25,19 +31,33 @@ public class Search_In_Bitonic {
         }
         return start;
     }
-    static int binarySearch(int[] arr, int target, int start, int end){
+    static int orderAgnosticBS(int[] arr, int target, int start, int end){
 
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        // find if the array is sorted in ascending or descending
 
-            if(target < arr[mid]){
-                end = mid - 1;
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start<=end){
+            // find the middle element
+            int mid = start + (end -start) / 2;
+
+            if(arr[mid] == target){
+                return mid;
             }
-            else if (target > arr[mid]) {
-                start = mid + 1;
+
+            if(isAsc){
+                if(target < arr[mid]){
+                    end = mid -1;
+                } else {
+                    start = mid + 1;
+                }
             }
             else {
-                return mid;
+                if (target > arr[mid]){
+                    end = mid -1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
         return -1;
